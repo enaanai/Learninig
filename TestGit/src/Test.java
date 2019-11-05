31c905ed6295b2b01c3817a09b9d012a9870f5c2
enaanai <enaanai@gmail.com>
2019-11-05 14:18:17 +0100

    Third

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		String n=sc.nextLine();
+		int age=Integer.parseInt(sc.nextLine());
 	System.out.println("Bonjour "+n);
 	}
 
